8f328dbc108773d9c132e5181026a5b01a4d736e
nathius262 <egbodonathaniel@yahoo.com>
2023-12-28 16:04:59 +0100

    modified user app

--- user/models.py
+++ user/models.py
@@ -7,3 +7,5 @@ class Profile(models.Model):
     user=models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="user_profile", null=True, blank=False)
     image = models.ImageField(upload_to=upload_location, null=True, blank=True)
     
+    def __str__(self):
+        return str(self.user)
